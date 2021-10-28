d3932d956d3db738ae0f9d690d626869fe38fce4
Niki Zadeh <Niki.Zadeh@noaa.gov>
2021-10-28 16:02:47 -0400

    Replace nan with 0 in angle_x
    - [arctan2](https://github.com/adcroft/numpypi/blob/1db49f1a68a9a72b3e46f0b290db740ea2d74310/numpypi_series.py#L284) returns np.nan when both arguments are 0. Why? I think it should return a number, like 0 or + pi/2 or -pi/2 .
    According to the following numpy doc it should return 0:
    https://numpy.org/doc/stable/reference/generated/numpy.arctan2.html
    
    This happens at 3 points in the tripolar grid when angle_x switches from +90 to -90 to +90 along x at the last y.
    NaN's in the netcdf cause md5 checksum hashes not to reproduce on different platforms (probably, I am not sure).

--- t/test_ocean_grid_gen.py
+++ t/test_ocean_grid_gen.py
@@ -16,12 +16,14 @@ def hashfile(f):
 class TestOGG():
 
   def test_hgrid_res4_0(self, tmpdir):
+    tic0 = time.perf_counter()
     outfile = tmpdir.join('ocean_hgrid_res4.0.nc')
     sp = subprocess.run([ogg_cmd,
                          '-f', outfile, '-r', '0.25',
                          '--even_j', '--no_changing_meta'])
     assert sp.returncode == 0
-    assert hashfile(outfile) == '47f5fc42e7f598f1d339c15e7fc88dc1d7b03956898294f4f185fe17b0a0f31d'
+    assert hashfile(outfile) == 'ccf9b9bcb789e57722b8af867079eb3ad6788da98f5fa855aeba029771e116a8'
+    print("res4 time(s): ",time.perf_counter() - tic0)
 
   def test_hgrid_res1_0(self, tmpdir):
     outfile = tmpdir.join('ocean_hgrid_res1.0.nc')
@@ -29,7 +31,7 @@ class TestOGG():
                          '-f', outfile, '-r', '1.0',
                          '--south_cutoff_row', '2', '--no_changing_meta'])
     assert sp.returncode == 0
-    assert hashfile(outfile) == 'bb57a86b788cb27c71ad139b5c72892fbe7019ffe774ecce4c0d74de54e678dc'
+    assert hashfile(outfile) == 'fd15681f5fb8ee81b7c9f7aa4f3a51291161b4c77a6594cae46db18043affc48'
 
   def test_hgrid_res0_5(self, tmpdir):
     outfile = tmpdir.join('ocean_hgrid_res0.5.nc')
@@ -37,7 +39,7 @@ class TestOGG():
                          '-f', outfile, '-r', '2',
                          '--no_changing_meta'])
     assert sp.returncode == 0
-    assert hashfile(outfile) == '87b29a240aa1ea9dbcce7d718a13704fa96b098650acbd99e4376884c35c4c83'
+    assert hashfile(outfile) == '2407058e0e2685b52c5cb6d5ca3c94341cacf6aea52c9e588ca08a13d7bbfd75'
   
   def test_hgrid_res0_5_equenh(self, tmpdir):
     outfile = tmpdir.join('ocean_hgrid_res0.5_equenh.nc')
@@ -46,7 +48,7 @@ class TestOGG():
                          '--south_cutoff_row', '130', '--no_changing_meta',
                          '--write_subgrid_files', '--enhanced_equatorial'])
     assert sp.returncode == 0
-    assert hashfile(outfile) == '683a42d5a155620456635dac57a2a6789058fd2a496feafb05db5b2c6015e754'
+    assert hashfile(outfile) == '223250ce5aa1f0bd2e3f976e84583a1b5836a4a8323e64119027fbc165e370a2'
 
   def test_hgrid_res0_25(self, tmpdir):
     outfile = tmpdir.join('ocean_hgrid_res0.25.nc')
@@ -54,13 +56,13 @@ class TestOGG():
                          '-f', outfile, '-r', '4', '--rdp', '0.2',
                          '--south_cutoff_row', '83', '--write_subgrid_files', '--no_changing_meta'])
     assert sp.returncode == 0
-    assert hashfile(outfile) == '12185aed9f1814c6b3c1545158338ee4160da96e2c9f389ffc31e1d3ed8c76ca'
+    assert hashfile(outfile) == '299f16cc397db0b1a595fb3d5d81465479452679a77530b6dc48a74e3c30af9b'
 
   def test_hgrid_res0_125(self, tmpdir):
     outfile = tmpdir.join('ocean_hgrid_res0.125.nc')
     sp = subprocess.run([ogg_cmd,
-                         '-f', outfile, '-r', '4', '--rdp', '0.2',
+                         '-f', outfile, '-r', '8', '--rdp', '0.2',
                          '--south_cutoff_row', '5', '--match_dy', '--even_j',
                          '--write_subgrid_files', '--no_changing_meta'])
     assert sp.returncode == 0
-    assert hashfile(outfile) == 'b1989adc7d7ae88da19aef546eebb2c96a1ad04202d2d1e3c3efd82aa3d693da'
+    assert hashfile(outfile) == 'fed9912406f2fda95fd7ab81a3b7b7c432c4a6834c9ba0f46afceab2e1ad6264'
