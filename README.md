aadb2b47681711979dbe864e14b8a24e4a29ea40
Niki Zadeh <Niki.Zadeh@noaa.gov>
2021-10-28 15:37:58 -0400

    Add a makefile for testing purposes

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ A collection of tools for creating finite element spherical tripolar grids for G
 
 Required python packages:
 - numpypi : python -m pip install git+https://github.com/underwoo/numpypi@pip.installable
+ 
+To test this module quickly try
+- make -f tests.mk quick
 
 [Technical guide](https://github.com/nikizadehgfdl/grid_generation/blob/dev/ocean_grid_generator_guide.pdf)
 
---
+++ tests.mk
@@ -0,0 +1,47 @@
+TARGS = ocean_hgrid_res4.0.nc \
+        ocean_hgrid_res1.0.nc \
+        ocean_hgrid_res0.5.nc \
+        ocean_hgrid_res0.5_equenh.nc \
+        ocean_hgrid_res0.25.nc \
+        ocean_hgrid_res0.125.nc 
+
+#Note: Github Travis cannot make the higher res grids below and errors out with "MemoryError"
+#      That is why they are commented out from TARGS so Travis can finish to keep records.   
+QUICKTARGS = ocean_hgrid_res4.0.nc \
+        ocean_hgrid_res1.0.nc \
+        ocean_hgrid_res0.5.nc \
+        ocean_hgrid_res0.5_equenh.nc
+
+all: $(TARGS) tests.md5
+	cat tests.md5
+	md5sum -c tests.md5
+
+quick : $(QUICKTARGS) 
+	head -4 tests.md5 > tests.md5.quick
+	md5sum -c tests.md5.quick
+
+#Note: --no_changing_meta arg is used to avoid putting time/platform dependent info in the files so that they can  be checksumed.
+#      Please do not use this arg for normal grid generation, it prevents adding useful information to meta data.
+ocean_hgrid_res4.0.nc:
+	time ./ocean_grid_generator.py $(DEBUG) -f ocean_hgrid_res4.0.nc -r 0.25 --even_j --no_changing_meta
+ocean_hgrid_res1.0.nc:
+	time ./ocean_grid_generator.py $(DEBUG) -f ocean_hgrid_res1.0.nc -r 1.0  --south_cutoff_row 2 --no_changing_meta
+ocean_hgrid_res0.5.nc: 
+	time ./ocean_grid_generator.py $(DEBUG) -f ocean_hgrid_res0.5.nc -r 2    --no_changing_meta 
+ocean_hgrid_res0.5_equenh.nc: 
+	time ./ocean_grid_generator.py $(DEBUG) -f ocean_hgrid_res0.5_equenh.nc -r 2 --south_cutoff_row 130 --no_changing_meta --write_subgrid_files --enhanced_equatorial
+ocean_hgrid_res0.25.nc:
+	time ./ocean_grid_generator.py $(DEBUG) -f ocean_hgrid_res0.25.nc -r 4 --rdp 0.2 --south_cutoff_row 83 --write_subgrid_files --no_changing_meta
+ocean_hgrid_res0.125.nc:
+	time ./ocean_grid_generator.py $(DEBUG) -f ocean_hgrid_res0.125.nc -r 8 --rdp 0.2 --south_cutoff_row 5 --match_dy --even_j --write_subgrid_files --no_changing_meta
+
+
+tests.md5: | $(TARGS)
+	md5sum $(TARGS) > $@
+	cat $@
+
+check:
+	md5sum -c tests.md5
+
+clean:
+	rm -f $(TARGS) $(DEPS) ocean_hgrid_res*.nc
