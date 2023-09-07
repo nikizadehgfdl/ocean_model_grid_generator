90a804926c5bbd83d95fea1f12730de6c7734890
Niki Zadeh <Niki.Zadeh@noaa.gov>
2023-09-07 08:58:46 -0400

    Fix jumps in dy
    - We introduce new arguments to match the latitude resolution between
      adjucent subgrids across their joint.
      User can specify which joints should be matched
        "--match_dy bp" for Mercator to bipolar cap joint
        "--match_dy so" for Mercator to Southern Ocean joint
        "--match_dy bp so" for both
        "--match_dy bp so p125sc" to reproduce CM4X

--- t/test_ocean_grid_gen.py
+++ t/test_ocean_grid_gen.py
@@ -39,7 +39,7 @@ class TestOGG:
         assert sp.returncode == 0
         assert (
             hashfile(outfile)
-            == "47f5fc42e7f598f1d339c15e7fc88dc1d7b03956898294f4f185fe17b0a0f31d" #PAN
+            == "0bc8e50b7d4110fcfb148f9f9590cf3c5e6c2d699b9c5519600aea7cc9483381" #PAN
             or hashfile(outfile)
             == "7e04457b4411260341f304a5182ec7f289ddb2724021e2825751f9994e23a6ed" #githubCI
         )
@@ -62,7 +62,7 @@ class TestOGG:
         assert sp.returncode == 0
         assert (
             hashfile(outfile)
-            == "bb57a86b788cb27c71ad139b5c72892fbe7019ffe774ecce4c0d74de54e678dc" #PAN 
+            == "6fed3c3f57894ea5e635c5412fdcdef90f44576908b10a894f06035c38159f9f" #PAN 
             or hashfile(outfile)
             == "ec24bbb7fd8b076d4d566777d2a924c4c6ac8f45bdf5b55c94eaeaa12ab07cbf" #githubCI
         )
@@ -83,7 +83,7 @@ class TestOGG:
         assert sp.returncode == 0
         assert (
             hashfile(outfile)
-            == "87b29a240aa1ea9dbcce7d718a13704fa96b098650acbd99e4376884c35c4c83" #PAN
+            == "cadb254616cee8c0fd8ffc52360de7d5b23ef298397e2873151b4c7e55d999b7" #PAN
             or hashfile(outfile)
             == "b2e8bbf65da5aa1de011119a269f4b3f236bf62ebe14778f1f458b552a1c1a9d" #githubCI
         )
@@ -98,8 +98,6 @@ class TestOGG:
                 outfile,
                 "-r",
                 "2.0",
-                "--south_cutoff_row",
-                "130",
                 "--no_changing_meta",
                 "--write_subgrid_files",
                 "--enhanced_equatorial=4",
@@ -108,7 +106,7 @@ class TestOGG:
         assert sp.returncode == 0
         assert (
             hashfile(outfile)
-            == "683a42d5a155620456635dac57a2a6789058fd2a496feafb05db5b2c6015e754" #PAN
+            == "26cee1438eb2a5188ef708528b53fca07bf2312b6cfd92f8e76dbf7f6c6ecaa0" #PAN
             or hashfile(outfile)
             == "30a040b8fbdf0683cc50d8ac2be841bb81f3f681e3b0c1a274f841e6fbd47d44" #githubCI
         )
@@ -134,12 +132,31 @@ class TestOGG:
         assert sp.returncode == 0
         assert (
             hashfile(outfile)
-            == "12185aed9f1814c6b3c1545158338ee4160da96e2c9f389ffc31e1d3ed8c76ca"  # gfdl-pan106,pan105
+            == "fd8ccd83cf5fff0d6c4a0a3abf7f5a4f561107ddf4f282f1289a113077ca6738"  # gfdl-pan106,pan105
             or hashfile(outfile)
             == "2d7840344aa356feb282d352bf21cce8832947b7989534290bd24d30dc561b70"  # github,gfdl-pan202
             or hashfile(outfile)
             == "1a0c3ca0e5b71ebdb8c68c63b2134c48a4bc8d9b9c9ba32f7298171a315d9508"  # githubCI
             #== "20315d58d80747559ed069fff521511624fed778ab92df6f9764503b79a15eea"  # githubCI old
+        )
+
+    def test_hgrid_res0_25_om5proto(self, tmpdir):
+        outfile = tmpdir.join("ocean_hgrid_res0.25_om5proto.nc")
+        sp = subprocess.run(
+            [
+                sys.executable,
+                ogg_cmd,
+                "-f", outfile, "-r","4",
+                "--south_ocean_lower_lat", "-88.57", "--match_dy", "so", "--no_south_cap",
+                "--write_subgrid_files","--no_changing_meta",
+            ]
+        )
+        assert sp.returncode == 0
+        assert (
+            hashfile(outfile)
+            == "0dd436ac1de339c447bd53a47f54cdff153a73a31e782bee59b1699209aa6b0d"  # gfdl-pan106,pan105
+            or hashfile(outfile)
+            == "1a0c3ca0e5b71ebdb8c68c63b2134c48a4bc8d9b9c9ba32f7298171a315d9508"  # githubCI
         )
 
 
